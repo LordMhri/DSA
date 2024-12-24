f19a6b1d40cd8817fccd0548abb207ccca57265f
lordmhri <meheret.alemu-ug@aau.edu.et>
2024-12-24 21:54:32 +0300

    added more option for deletion

--- labfiles/BinarySearchTree.cpp
+++ labfiles/BinarySearchTree.cpp
@@ -22,8 +22,33 @@ public:
     }
 
     void deleteNode(int value)
+    {   
+        int choice;
+        cout << "1) Delete From Max Left\n";
+        cout << "2) Delete From Min Right\n";
+        cin >> choice;
+        if (choice == 1)
+        {
+            deleteRec(root,value,1);
+        } else {
+            deleteRec(root,value,2);
+        }
+    }
+
+    void deleteByMerge(int value)
     {
-        root = deleteRec(root, value);
+        int choice;
+        cout << "1) Delete From Max Left\n";
+        cout << "2) Delete From Min Right\n";
+        cin >> choice;
+        if (choice == 1)
+        {
+            deleteMerge(root, value, 1);
+        }
+        else
+        {
+            deleteMerge(root, value, 2);
+        }
     }
 
     void printInorderTraversal()
@@ -38,41 +63,49 @@ public:
         cout << endl;
     }
 
-    int height(){
+    int height()
+    {
         return heightOfTree(root);
     }
 
-    
-    
-    int depthOfANode(Node* node, int data, int depth = 0) {
-        if (node == nullptr) {
-            return -1; // Node not found
-        }
-        if (node->data == data) {
-            return depth;
-        }
-        int leftDepth = depthOfANode(node->left, data, depth + 1);
-        if (leftDepth != -1) {
-            return leftDepth;
-        }
-        return depthOfANode(node->right, data, depth + 1);
-    }
-
-    void depthOfANode(int data){
-        int depth = depthOfANode(root, data);
-        if (depth != -1) {
+    void depthOfANode(int data)
+    {
+        int depth = depthOfANode(root, data, 0);
+        if (depth != -1)
+        {
             cout << "Depth of node with data " << data << " is: " << depth << endl;
-        } else {
+        }
+        else
+        {
             cout << "Node with data " << data << " not found in the tree." << endl;
         }
     }
 
-    void printPreOrderTraversal(){
+    void printPreOrderTraversal()
+    {
         preOrderTraversal(root);
         cout << endl;
     }
 
 private:
+    int depthOfANode(Node *node, int data, int depth = 0)
+    {
+        if (node == nullptr)
+        {
+            return -1; // Node not found
+        }
+        if (node->data == data)
+        {
+            return depth;
+        }
+        int leftDepth = depthOfANode(node->left, data, depth + 1);
+        if (leftDepth != -1)
+        {
+            return leftDepth;
+        }
+        return depthOfANode(node->right, data, depth + 1);
+    }
+
     int heightOfTree(Node *root)
     {
         if (!root)
@@ -81,14 +114,7 @@ private:
         }
         int left_height = heightOfTree(root->left);
         int right_height = heightOfTree(root->right);
-        if (left_height >= right_height)
-        {
-            return left_height + 1;
-        }
-        else
-        {
-            return right_height + 1;
-        }
+        return max(left_height, right_height) + 1;
     }
 
     Node *insertRec(Node *node, int data)
@@ -108,7 +134,7 @@ private:
         return node;
     }
 
-    Node *deleteRec(Node *root, int value)
+    Node *deleteRec(Node *root, int value,int choice)
     {
         if (root == nullptr)
         {
@@ -116,11 +142,11 @@ private:
         }
         if (root->data < value)
         {
-            root->right = deleteRec(root->right, value);
+            root->right = deleteRec(root->right, value,choice);
         }
         else if (root->data > value)
         {
-            root->left = deleteRec(root->left, value);
+            root->left = deleteRec(root->left, value,choice);
         }
         else
         {
@@ -136,40 +162,67 @@ private:
                 delete root;
                 return temp;
             }
-            Node *temp = minValueNode(root->right);
+            Node *temp;
+            if (choice == 1) {
+                temp = minValueNode(root->right);
+            } else {
+                temp = maxValueNode(root->left);
+            }            
             root->data = temp->data;
-            root->right = deleteRec(root->right, temp->data);
+            root->right = deleteRec(root->right, temp->data,choice);
         }
         return root;
     }
-
-    Node *deleteMerge(Node*root,int value){
+    Node *deleteMerge(Node *root, int value, int choice)
+    {
         if (root == nullptr)
         {
             return root;
-        } else if(root->left == nullptr){
-            root->right = deleteMerge(root->right,value);
-        } else if(root->right == nullptr){
-            root->left = deleteMerge(root->left,value);
-        } else {
-            if (root->left ==nullptr)
+        }
+
+        if (value < root->data)
+        {
+            root->left = deleteMerge(root->left, value, choice);
+        }
+        else if (value > root->data)
+        {
+            root->right = deleteMerge(root->right, value, choice);
+        }
+        else
+        {
+            if (root->left == nullptr)
             {
                 Node *temp = root->right;
                 delete root;
-                return temp; 
-            } else if (root->right == nullptr){
+                return temp;
+            }
+            else if (root->right == nullptr)
+            {
                 Node *temp = root->left;
                 delete root;
                 return temp;
             }
-
-            Node *temp = minValueNode(root->right);
-            temp->right = root->right;
-            return root->left;
+            else
+            {
+                Node *temp;
+                if (choice == 1)
+                {
+                    temp = minValueNode(root->right);
+                    root->data = temp->data;
+                    root->right = deleteMerge(root->right, temp->data, choice);
+                }
+                else
+                {
+                    temp = maxValueNode(root->left);
+                    root->data = temp->data;
+                    root->left = deleteMerge(root->left, temp->data, choice);
+                }
+            }
         }
+
         return root;
     }
-
+   
     Node *minValueNode(Node *root)
     {
         Node *current = root;
@@ -227,42 +280,92 @@ private:
 int main()
 {
     BinarySearchTree Btree;
-    int num = 4096;
+    int arr[7] = {10, 5, 3, 7, 12, 15, 18};
 
-    for (int i = 1; i < 10; i++)
+    for (int i = 0; i < 7; i++)
     {
-        Btree.insert(i + 1);
+        Btree.insert(arr[i]);
     }
 
-    for (int i = 0; i < 10; i++)
-    {
-        num = num / 16;
-        Btree.insert(num);
-    }
-
+    cout << "These are the values in the tree now\n" << endl;
     Btree.printInorderTraversal();
 
-    cout << "Now post order\n";
-    Btree.printPostOrderTraversal();
+    
+    int choice, value;
+    BinarySearchTree Copy;
+    while (true)
+    {
+        cout << "Menu:\n";
+        cout << "1. Inorder Traversal\n";
+        cout << "2. Preorder Traversal\n";
+        cout << "3. Postorder Traversal\n";
+        cout << "4. Delete Node from a Copy Tree (Copy)\n";
+        cout << "5. Delete Node from a Copy Tree (Merge) \n";
+        cout << "6. Delete Node from real Tree Completely\n";
+        cout << "7. Height of Tree\n";
+        cout << "8. Depth of a Node\n";
+        cout << "9. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-    Btree.deleteNode(121312);
-
-    cout << "Now pre order\n";
-    Btree.printPreOrderTraversal();
-
-    Btree.depthOfANode(16);
-
-    cout << "Height of tree is " << Btree.height() << "\n";
+        switch (choice)
+        {
+        case 1:
+            Btree.printInorderTraversal();
+            break;
+        case 2:
+            Btree.printPreOrderTraversal();
+            break;
+        case 3:
+            Btree.printPostOrderTraversal();
+            break;
+        case 4:
+            cout << "Removing from a copy tree\n";
+            cout << "Enter value to delete: ";
+            cin >> value;
+            Copy = Btree;
+            Copy.deleteNode(value);
+            cout << "Height of tree After Deletion by Copying: " << Btree.height() << endl;
+            cout << "InOrderTraversal After Deletion: ";
+            Copy.printInorderTraversal();
+            break;
+        case 5:
+            cout <<"Removing from a copy tree\n";
+            cout << "Enter value to delete: ";
+            cin >> value;
+            Copy = Btree;
+            Btree.deleteByMerge(value);
+            cout << "Height of tree After Deletion by Copy: " << Copy.height() << endl;
+            cout << "InOrderTraversal After Deletion: ";
+            Copy.printInorderTraversal();
+            break;
+        case 6:
+            cout << "Remove From original Tree\n";
+            cout << "Enter value to delete: ";
+            cin >> value;
+            Btree.deleteNode(value);
+            cout << "Height of tree After Deletion by Merge: " << Btree.height() << endl;
+            cout << "InOrderTraversal After Deletion\n";
+            Btree.printInorderTraversal();
+            break;
+        case 7:
+            cout << "Height of tree: " << Btree.height() << endl;
+            break;
+        case 8:
+            cout << "Enter value to find depth: ";
+            cin >> value;
+            Btree.depthOfANode(value);
+            break;
+        case 9:
+            return 0;
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    }
+   
 
     return 0;
 }
+// #share sort
+// #quick sort
 
-/*
-                        9
-
-                    8       12
-                        11
-
-
-
-*/
